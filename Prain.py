a9d62b8fc1726404812712c8526f7a8716ad4ed1
Franz Liszt <47530146+BigDaddyLiszt@users.noreply.github.com>
2019-06-14 12:29:48 +0100

    Add files via upload

--- Prain.py
+++ Prain.py
@@ -14,13 +14,13 @@ from Fluid import Fluid
 
 def DragForce(sperm, fluid):
     #Drag Force, duh.
-    return -1/(6*math.pi*fluid.viscocity*sperm.length)
+    DragForce = -((6*math.pi*fluid.viscocity*sperm.length))*(sperm.velocity)
+    print(DragForce)
+    return -((6*math.pi*fluid.viscocity*sperm.length))*(sperm.velocity)
 
 def SpinDragForce(sperm, fluid):
     #Rotational Drag Force, duh.
     return -1/(8*math.pi*fluid.viscocity*(sperm.lenth)**3)
-
-
 
 def main():
 
@@ -44,8 +44,11 @@ def main():
 
     #Initialising Sperm and Fluid Objects
 
-    sperm = Spermatozoon(np.array([0,0,0]),np.array([random.randint(0,50),random.randint(0,50),random.randint(0,50)]), 1, leg, np.array([0]), 0)
+    sperm = Spermatozoon(np.array([0.0001,0,0]),np.array([random.randint(0,500),random.randint(0,500),random.randint(0,500)]), 80, leg, np.array([0]), 1)
     fluid = Fluid(visc, speed, den)
+
+    sperm_position_list = []
+
 
     # Set up simulation parameters
     dt = 0.01
@@ -53,15 +56,37 @@ def main():
     time = 0.0
 
     #Initial Force Value
-    Force = DragForce(sperm, fluid)
+    force = DragForce(sperm, fluid)
 
-    #Initial Time List
+    #Initial Time and Position List
     time_list = [0.0]
+    pos_list = [sperm.position]
+
 
     #Hopefully I can visulise it with this
     f = open(output_file,'w') #Whatever Programme I use to model this
 
-    
+    # Start the time integration loop
+    for t in range(numstep):
 
+        #Position Update
+        sperm.leap_pos2nd(dt, force)
+
+        #Re-calculating Force
+        force_new = DragForce(sperm, fluid)
+
+        #Velocity Update
+        sperm.leap_velocity(dt, force + force_new)
+
+        # Increase time and reset force value
+        time += dt
+        pos_list.append(sperm.position)
+        time_list.append(time)
+        force = force_new
+
+        #Write a VMD file
+        f.write(str(1)+"\n")
+        f.write("Point = " + str(t+1) +"\n")
+        f.write("s"+ str(1) + " " + str(sperm.position[0]) + " " + str(sperm.position[1]) + " " + str(sperm.position[2]) + "\n")
 
 main()

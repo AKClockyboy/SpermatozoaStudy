26196fb8f6ab3fd958982295af52c10b26ca8e1c
Franz Liszt <47530146+BigDaddyLiszt@users.noreply.github.com>
2019-06-19 12:17:59 +0100

    Add files via upload

--- PDF.py
+++ PDF.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import scipy.stats
 
-def PDF2(n_bins, vector_magnitude_list):
+def PDF(n_bins, vector_magnitude_list):
     shape,loc,scale=scipy.stats.lognorm.fit(vector_magnitude_list,floc=0)
     clr="#EFEFEF"
     counts,edges,patches=plt.hist(vector_magnitude_list,bins=n_bins,color=clr)
@@ -23,3 +23,33 @@ def PDF2(n_bins, vector_magnitude_list):
     plt.xlabel("Magnitude of Vector")
     plt.ylabel("Probability")
     plt.plot(centers, len(vector_magnitude_list)*prob,'k-',linewidth=2)
+
+def RandSphere(N, vector_list, ax):
+    #Setting up parametres
+    phi = np.random.uniform(0,np.pi*2)
+    theta = np.arccos(np.random.uniform(-1,1))
+
+    #Creating x, y and z coordinates of the vectors
+    x = np.sin(theta) * np.cos(phi)
+    y = np.sin(theta) * np.sin(phi)
+    z = np.cos(theta)
+
+    #Actualises vector as an array then appends to list --- factor of 1/N ot factorise
+    vector = [-x/N, -y/N, -z/N]
+    vector_list.append(vector)
+
+    #Plot every vector individually
+    ax.scatter(x, y, z, c='b', marker = '3')
+
+    return(vector_list)
+
+def MagnitudeSum(vector_list, vector_magnitude_list, ax, origin):
+    #Now adding N vectors together to obtain magnitude
+    vector_sum = (list(map(sum, zip(*vector_list))))
+    vector_magnitude = (vector_sum[0]*vector_sum[0] + vector_sum[1]*vector_sum[1] + vector_sum[2]*vector_sum[2])**1/2
+    vector_magnitude_list.append(vector_magnitude)
+
+    #Plotting for displaying the line of force
+    X, Y, Z = zip(origin)
+    U, V, W = zip(vector_sum)
+    ax.quiver(X,Y,Z,U,V,W,arrow_length_ratio = 0.1)
--- VectorGen.py
+++ VectorGen.py
@@ -19,45 +19,20 @@ ax = plt.axes(projection='3d')
 plt.title("Randomly distributed flagella vectors on a sphere", fontdict=None, loc='center', pad=None)
 
 #Constants and Parametres
-iterations = 100
+iterations = 10000
 vector_magnitude_list = []
 origin = [0,0,0]
-N = 5
-n_bins = 50
+N = 16
+n_bins = 100
 
 for i in range(iterations):
     vector_list = []
-    #Loop for number of vectors we want
     for i in range(N):
-        #Setting up parametres
-        phi = np.random.uniform(0,np.pi*2)
-        theta = np.arccos(np.random.uniform(-1,1))
+        pdff.RandSphere(N, vector_list, ax)
 
-        #Creating x, y and z coordinates of the vectors
-        x = np.sin(theta) * np.cos(phi)
-        y = np.sin(theta) * np.sin(phi)
-        z = np.cos(theta)
+    pdff.MagnitudeSum(vector_list, vector_magnitude_list, ax, origin)
 
-        #Actualises vector as an array then appends to list --- factor of 1/N ot factorise
-        vector = [-x/N, -y/N, -z/N]
-        vector_list.append(vector)
-
-        #Plot every vector individually
-        ax.scatter(x, y, z, c='b', marker = '3')
-
-    #Now adding N vectors together to obtain magnitude
-    vector_sum = (list(map(sum, zip(*vector_list))))
-    vector_magnitude = (vector_sum[0]*vector_sum[0] + vector_sum[1]*vector_sum[1] + vector_sum[2]*vector_sum[2])**1/2
-    vector_magnitude_list.append(vector_magnitude)
-
-
-    #Plotting for displaying the line of force
-    X, Y, Z = zip(origin)
-    U, V, W = zip(vector_sum)
-    ax.quiver(X,Y,Z,U,V,W,arrow_length_ratio = 0.01)
 plt.show()
 
-print("vector mag list is " + str(vector_magnitude_list))
-
-pdff.PDF2(n_bins, vector_magnitude_list)
+pdff.PDF(n_bins, vector_magnitude_list)
 plt.show()

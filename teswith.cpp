9f907aa1eaf4604e7849649809e49bdc2cdc5fcc
Franz Liszt <47530146+BigDaddyLiszt@users.noreply.github.com>
2019-07-02 16:00:30 +0100

    Add files via upload

--- teswith.cpp
+++ teswith.cpp
@@ -35,13 +35,75 @@ int main()
 }
 */
 
-//Trying to define a function!
+//Trying to define a function! Adding together two numbers!
 
-int add(int, int);
+/*
+int add(int, int); //Declaration (?)
+int main()
+{
+  int n1, n2,sum;
+  cout << "Enter two numbers... the programme will add them together: ";
+  cin >> n1 >> n2;
+
+  sum = add(n1, n2);
+
+  cout << "The sum of the two entered numbers, "<<n1<<" and "<<n2<<", is " << sum;
+
+  return 0;
+
+}
+
+int add(int a, int b) // Actually DEFINING the funciton now??
+{
+    int add;
+    add = a + b;
+    // Return statement
+    return add;
+}
+*/
+
+/*
+//Function Over loading
+void display(int);
+void display(float);
+void display(int, float); //Declaring prototypes, this time 3 functions have the same name
 
 int main()
 {
-  int (n1, n2,sum)
-  cout << "Enter two numbers... the programme will add them together: "
-  cin >> n1 >> n1
+  int a = 6;
+  float b = 6.78;
+  display(a);
+  display(b);
+  display(a,b);
+
+  return 0;
+
 }
+
+void display(int var)
+
+{
+
+  cout << "Da intaga' is dis' mon' : " << var << endl;
+
+}
+
+
+void display(float var)
+
+{
+
+  cout << "Da floatin' ting is dis' mon' : " << var << endl;
+
+}
+
+
+void display(int var1,float var2)
+
+{
+
+  cout << "Da intaga' is dis' mon' : " << var1;
+  cout << " and da floatin' ting is dis' mon : " << var2;
+
+}
+*/

b22a1ba4d20179014dd7b020eb74f656ea2bdda1
Franz Liszt <47530146+BigDaddyLiszt@users.noreply.github.com>
2019-07-02 15:05:04 +0100

    Add files via upload

--- teswith.cpp
+++ teswith.cpp
@@ -1,13 +1,47 @@
-// Program to print positive number entered by the user
-// If user enters negative number, it is skipped
+//Space for testing C++ things
 
-#include <isostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
+
+/*
+int main()
+{
+    int n, factorial = 1;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        factorial *= i;   // factorial = factorial * i;
+    }
+    cout<< "Factorial of "<<n<<" = "<<factorial;
+    return 0;
+}
+*/
+
+/* SQUARE ROOT CALCULATION
+int main()
+{ double n,squarert;
+  cout << "Enter an number and this should return the square root! : ";
+  cin >> n;
+
+  //Now the calculation...
+
+  squarert = sqrt(n);
+
+  cout << "Ya square root of "<<n<<" is " << squarert;
+
+  return 0;
+}
+*/
+
+//Trying to define a function!
+
+int add(int, int);
 
 int main()
 {
-  
-  cout<<"\n\nHello World, \nThis is an attempt to write in C++\n\n"<<endl;
-  return 0;
-
+  int (n1, n2,sum)
+  cout << "Enter two numbers... the programme will add them together: "
+  cin >> n1 >> n1
 }

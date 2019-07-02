2c1217cc88379bfaeec7283d7df519d0d5c4a79c
Franz Liszt <47530146+BigDaddyLiszt@users.noreply.github.com>
2019-07-02 13:21:29 +0100

    Add files via upload

---
+++ teswith.cpp
@@ -0,0 +1,13 @@
+// Program to print positive number entered by the user
+// If user enters negative number, it is skipped
+
+#include <isostream>
+using namespace std;
+
+int main()
+{
+  
+  cout<<"\n\nHello World, \nThis is an attempt to write in C++\n\n"<<endl;
+  return 0;
+
+}
